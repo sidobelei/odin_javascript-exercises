eaaa46d2684ebda66b5f9ffc148f27b0e210de68
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-04-02 16:22:01 -0700

    Add function to repeat strings to repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeatedString = '';
+    if (num >= 0) { 
+        for (let i = num; i > 0; i--) {
+            repeatedString = repeatedString + string;
+        }
+        return repeatedString;
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
