0a34ec4677debeb758a2a3199ff1fa1df312f5af
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-04-03 14:29:05 -0700

    Add removeFromArray function to removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removedNums) {
+    const newArray = [];
+    let match = false;
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < removedNums.length; j++) {
+            if (array[i] === removedNums[j]) {
+                match = true;
+            }
+        }
+        if (match === false) {
+            newArray.push(array[i]);
+        }
+        else {
+            match = false;
+        }
+    }
+    console.log(newArray);
+    return newArray;
 };
 
 // Do not edit below this line
