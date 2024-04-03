148af303ef03f9fd2cfe34059f3e33212f391d96
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-04-03 16:29:05 -0700

    Add tempConversion function to tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * (5/9);
+  celsius = celsius.toFixed(1);
+  celsius = Number(celsius);
+  return celsius;  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = celsius * (9/5) + 32;
+  fahrenheit = fahrenheit.toFixed(1);
+  fahrenheit = Number(fahrenheit);
+  return fahrenheit;
 };
 
 // Do not edit below this line
