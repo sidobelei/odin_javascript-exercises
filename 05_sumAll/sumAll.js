929efdc109d9ebef07aaf7fd88773404e139d937
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-04-03 15:21:11 -0700

    Add sumAll function to sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(node1, node2) {
+    let sum = 0;
+    let start;
+    let end;
+    let nodes = [node1, node2];
+    for (const node of nodes) {
+        if (Number.isInteger(node) === false || node < 0) {
+            return "ERROR";
+        }
+    }
 
+    if (node1 < node2) {
+        start = node1;
+        end = node2;
+    }
+    else {
+        start = node2;
+        end = node1;
+    }
+
+    for (let i = start; i <= end; i++) {
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
