a8d70630f1a96cd4fc2e90185db732554ec5805a
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-04-19 10:55:32 -0700

    Add fibonacci generator function to fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index < 0) {
+        return "OOPS";
+    }
+    else if (isNaN(index) === false) {
+        index = Number(index);
+    }
 
+    let sequence = [0, 1];
+    if (index > 1) {
+        for (let i = 2; i < index + 1; i++) {
+            sequence.push(sequence[i - 1] + sequence[i - 2]);
+        }
+    }
+    return sequence[index];
 };
 
 // Do not edit below this line
