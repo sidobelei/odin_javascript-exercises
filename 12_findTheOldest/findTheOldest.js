c0e3bb1f29d15cb3ec490f3c506a5bddab26e3a8
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-04-19 10:58:49 -0700

    Add findTheOldest function to findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.sort(function (a, b) {
+        if ("yearOfDeath" in a === false) {
+            a.yearOfDeath = new Date().getFullYear();
+        }
+        if ("yearOfDeath" in b === false) {
+            b.yearOfDeath = new Date().getFullYear();
+        }
+        return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth) 
+    })
+    return people[0]
 };
 
 // Do not edit below this line
