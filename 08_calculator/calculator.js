2ace84b01e608dde7f3c7738aae87bc06d0f63c6
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-04-19 10:51:20 -0700

    Add mathematical functions to calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	if (array.length === 0) {
+    return 0;
+  }
+  else {
+    return array.reduce((total, currentVal) => total + currentVal);
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	if (array.length === 0) {
+    return 0;
+  }
+  else {
+    return array.reduce((total, currentVal) => total * currentVal);
+  }
 };
 
-const power = function() {
-	
+const power = function(operand, toPower) {
+	return operand ** toPower;
 };
 
-const factorial = function() {
-	
+const factorial = function(operand) {
+	if (operand === 0) {
+    return 1;
+  }
+  let operands = [];
+  for (let i = 1; i <= operand; i++) {
+    operands.push(i);
+  }
+  return operands.reduce((total, currentValue) => total * currentValue);
 };
 
 // Do not edit below this line
