72a75b833ff36510aec9fff65bd65c8ebe39c548
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-04-19 10:52:43 -0700

    Add palindromes checker function to palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase().replaceAll(".", "").replaceAll(",", "").replaceAll(" ", "").replaceAll("!", "");
+    let reversed = string;
+    reversed = reversed.split("").reverse().join("");
+    return reversed === string;
 };
 
 // Do not edit below this line
